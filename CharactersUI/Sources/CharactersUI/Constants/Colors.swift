2c560f72bcd3d95d716e31206246c7398af8f8ce
omarmohabdelmaksoud <omar.abdelmaksoud@glovoapp.com>
2025-08-23 00:07:24 +0200

    adapted colors for dark mode

--- CharactersUI/Sources/CharactersUI/Constants/Colors.swift
+++ CharactersUI/Sources/CharactersUI/Constants/Colors.swift
@@ -1,8 +1,22 @@
 import SwiftUI
 
 extension Color {
-  static let lightBlue = Color(red: 0.85, green: 0.93, blue: 1.0)
-  static let lightRed = Color(red: 1.0, green: 0.90, blue: 0.90)
+  static var lightBlue: Color {
+    Color(UIColor { traitCollection in
+      traitCollection.userInterfaceStyle == .dark
+      ? UIColor(red: 0.15, green: 0.25, blue: 0.35, alpha: 1)
+      : UIColor(red: 0.85, green: 0.93, blue: 1.0, alpha: 1)
+    })
+  }
+
+  static var lightRed: Color {
+    Color(UIColor { traitCollection in
+      traitCollection.userInterfaceStyle == .dark
+      ? UIColor(red: 0.25, green: 0.15, blue: 0.15, alpha: 1)
+      : UIColor(red: 1.0, green: 0.90, blue: 0.90, alpha: 1)
+    })
+  }
+  
   static let lightGray = Color.gray.opacity(0.3)
   
 }
@@ -13,7 +27,7 @@ extension Color {
     enum Status {
       static let alive = Color.lightBlue
       static let dead = Color.lightRed
-      static let unknown = Color.white
+      static let unknown = Color(.systemBackground)
     }
     
     enum Border {
