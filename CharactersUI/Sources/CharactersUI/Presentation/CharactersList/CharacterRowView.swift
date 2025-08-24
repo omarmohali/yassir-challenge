81de7f386a691654896a82e8a4966a5fe1008ce0
omarmohabdelmaksoud <omar.abdelmaksoud@glovoapp.com>
2025-08-24 16:29:41 +0200

    moved hosting to seperate file

--- CharactersUI/Sources/CharactersUI/Presentation/CharactersList/CharacterRowView.swift
+++ CharactersUI/Sources/CharactersUI/Presentation/CharactersList/CharacterRowView.swift
@@ -77,33 +77,6 @@ struct CharacterRowView: View {
   }
 }
 
-class CharacterTableViewCell: UITableViewCell {
-    private var hostingController: UIHostingController<CharacterRowView>?
-
-    func set(character: Character, parent: UIViewController) {
-      hostingController?.willMove(toParent: nil)
-      hostingController?.view.removeFromSuperview()
-      hostingController?.removeFromParent()
-
-      let hc = UIHostingController(rootView: CharacterRowView(character: character))
-      hc.view.backgroundColor = .clear
-
-      parent.addChild(hc)
-      contentView.addSubview(hc.view)
-
-      hc.view.translatesAutoresizingMaskIntoConstraints = false
-      NSLayoutConstraint.activate([
-        hc.view.topAnchor.constraint(equalTo: contentView.topAnchor),
-        hc.view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-        hc.view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-        hc.view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
-      ])
-
-      hc.didMove(toParent: parent)
-      hostingController = hc
-    }
-}
-
 struct CharacterRowView_Previews: PreviewProvider {
     static var previews: some View {
       CharacterRowView(
